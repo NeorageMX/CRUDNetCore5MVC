2a62600a8356c57043a6cd464d3d8099c380df88
NeorageMX <jairmiranda@outlook.com>
2021-06-19 17:51:42 -0500

    Add project files.

---
+++ CRUDNetCore5MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDNetCore5MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
